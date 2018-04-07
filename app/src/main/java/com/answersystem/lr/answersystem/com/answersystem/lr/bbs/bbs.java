7d69d7bfce64ebe193a48505ce3e9085579a743d
jojo <763124078@qq.com>
2018-04-07 13:33:12 +0800

    框架

---
+++ app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/bbs/bbs.java
@@ -0,0 +1,15 @@
+package com.answersystem.lr.answersystem.com.answersystem.lr.bbs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.answersystem.lr.answersystem.R;
+
+public class bbs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bbs);
+    }
+}
---
+++ app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/log_in/log_in.java
@@ -0,0 +1,15 @@
+package com.answersystem.lr.answersystem.com.answersystem.lr.log_in;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.answersystem.lr.answersystem.R;
+
+public class log_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
