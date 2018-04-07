bd9deec21c1c98c51213401e0fe19b31ffa86aa0
moeisjustice <33977715+moeisjustice@users.noreply.github.com>
2018-04-07 13:58:24 +0800

    Update home.java

--- app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/home/home.java
+++ app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/home/home.java
@@ -11,5 +11,8 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        
+        
     }
 }
