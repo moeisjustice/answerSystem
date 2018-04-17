008071439f5240d9fff1c45aea0ce1dae5eaa0d6
jojo <763124078@qq.com>
2018-04-17 21:50:17 +0800

    框架

--- app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/home/home.java
+++ app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/home/home.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 
 import com.answersystem.lr.answersystem.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        
-        int a;
-
+        CircleImageView circleImageView=(CircleImageView) findViewById(R.id.image);
     }
 }
